63801b3579a10af4db42cea85981115cc1c41142
Vika <viktoriav.aero@gmail.com>
2024-03-05 11:20:32 +0200

    unused code deleted in notifications

--- src/pages/Notifications/Notifications.tsx
+++ src/pages/Notifications/Notifications.tsx
@@ -1,29 +1,9 @@
 import React from "react"
-import { useAppDispatch } from "src/redux/hooks"
 import s from "./Notifications.module.scss"
 import AdminLayout from "src/layouts/AdminLayout"
 import NotificationsTabs from "../../components/NotificationsComponents/NotificationsTabs/NotificationsTabs.tsx"
 
 const Notifications: React.FC = () => {
-
-  const dispatch = useAppDispatch()
-  // const [isOpenModal, setIsOpenModal] = useState(false)
-  //
-  // const [page, setPage] = useState(0)
-  // const [pageSize, setPageSize] = useState(10)
-  // const [searchName, setSearchName] = useState("")
-  //
-  // useEffect(() => {
-  //   dispatch(
-  //     getBrands({
-  //       lookup: searchName,
-  //       pageSize,
-  //       page: page + 1,
-  //     })
-  //   )
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [page, pageSize, searchName])
-
   return (
     <AdminLayout>
       <main className={s.notification}>
