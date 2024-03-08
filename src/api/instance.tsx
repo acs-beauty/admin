cf041d023b5a94396713cde9a6d6f8f06dce296f
Konstantin Ligus <ligus.konstantin@gmail.com>
2024-03-08 11:30:03 +0200

    add withCredentials field option withtin instance

--- src/api/instance.tsx
+++ src/api/instance.tsx
@@ -4,6 +4,7 @@ const { VITE_API_BASE_URL } = import.meta.env
 
 export const instance = axios.create({
   baseURL: VITE_API_BASE_URL,
+  withCredentials: true,
 })
 
 // instance.interceptors.request.use(config => {
@@ -18,7 +19,7 @@ instance.interceptors.response.use(
     if (error.response.status === 401 && !originalRequest?._isRetry) {
       originalRequest._isRetry = true
       try {
-        await axios.get(`${VITE_API_BASE_URL}/user/refresh`)
+        await axios.get(`${VITE_API_BASE_URL}user/refresh`)
         return instance.request(originalRequest)
       } catch (error) {
         console.log("Not authorized")
